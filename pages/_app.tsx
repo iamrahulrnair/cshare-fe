09d823b26e0ab393054c6a0fe1f2720cafad92a9
iamrahulrnair <rahulrnair007@outlook.com>
2022-08-19 16:25:13 +0530

    add custom getInitial prop for global auth view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,37 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import type { AppProps } from 'next/app';
-
+import App from 'next/app';
+import axios from 'axios';
 import '../styles/globals.scss';
 import AuthContextProvider from '../context/utils/auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, authUser }: any) {
   return (
     <div>
       <AuthContextProvider>
-        <Component {...pageProps} />
+        <Component {...pageProps} authUser={authUser} />
         <ToastContainer />
       </AuthContextProvider>
     </div>
   );
 }
+
+MyApp.getInitialProps = async (appContext: any) => {
+  const pageProps = await App.getInitialProps(appContext);
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/account/me', {
+      withCredentials: true,
+    });
+    return {
+      ...pageProps,
+      authUser: { ...response.data, isAuthenticated: true },
+    };
+  } catch (err) {
+    return {
+      ...pageProps,
+      authUser: { isAuthenticated: false },
+    };
+  }
+};
 
 export default MyApp;

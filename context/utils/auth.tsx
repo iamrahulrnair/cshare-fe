54576ac42478eb5b785754759fa406647c32fbba
iamrahulrnair <rahulrnair007@outlook.com>
2022-08-19 16:26:22 +0530

    add token context

--- context/utils/auth.tsx
+++ context/utils/auth.tsx
@@ -8,9 +8,13 @@ export default function AuthContextProvider({
   children: React.ReactNode;
 }) {
   const [csrf, setCsrf] = useState(null);
+  const [authUser, setAuthUser] = useState({});
+
   const state = {
     csrf,
     setCsrf,
+    authUser,
+    setAuthUser,
   };
   return (
     <AuthContext.Provider value={{ ...state }}>{children}</AuthContext.Provider>

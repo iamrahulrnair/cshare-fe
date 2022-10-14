62cc474e98b9e24885f33d22efc6369e2468289d
iamrahulrnair <rahulrnair007@outlook.com>
2022-10-14 18:58:30 +0530

    add prism code editor/view component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,42 @@
 import axios from 'axios';
 import type { NextPage } from 'next';
-import { getCookie } from '../utils/auth';
-import { getAuthSession } from '../utils/lib/session';
+import CodeBlock from '../components/code/CodeBlock';
 
 import _ from 'lodash';
+import Link from 'next/link';
 
-const Home: NextPage = ({ authUser }: any) => {
+const Home: NextPage = ({ authUser, data }: any) => {
+  console.log(data);
+
   return (
-    <div className='text-[6rem]'>
-      username:{authUser?.username}
-      <button
-        onClick={async () => {
-          await axios.post(
-            'http://127.0.0.1:8000/api/account/logout/',
-            {},
-            {
-              withCredentials: true,
-              headers: {
-                'X-CSRFToken': getCookie('csrftoken')!,
-              },
-            }
-          );
-        }}
-      >
-        click me
-      </button>
-    </div>
+    data.length > 0 &&
+    data.map((el: any) => (
+      <Link href={`/code/${el.id}`}>
+        <div
+          key={el.id}
+          className='cursor-pointer mx-auto w-[100rem] mt-20 flex flex-col gap-10'
+        >
+          <CodeBlock code={JSON.parse(el.code)} language={el.extension} />
+        </div>
+      </Link>
+    ))
   );
 };
-// export async function getServerSideProps({ req }: any) {
-//   try {
-//     const data = await getAuthSession(req);
+export async function getServerSideProps({ req }: any) {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/code');
 
-//     return {
-//       props: {
-//         data,
-//       },
-//     };
-//   } catch (err: any) {
-//     return {
-//       redirect: {
-//         destination: '/auth/login',
-//         permanent: false,
-//       },
-//       props: {},
-//     };
-//   }
-// }
+    return {
+      props: {
+        data: response.data,
+      },
+    };
+  } catch (err: any) {
+    return {
+      props: {
+        error: 'Somthing went wrong',
+      },
+    };
+  }
+}
 export default Home;

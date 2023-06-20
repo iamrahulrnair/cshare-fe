335a757610c3d51e05a1dd7efee9f0319415c4fe
iamrahulrnair <rahulrnair007@outlook.com>
2023-06-20 23:16:04 +0530

    add debounce commit for search throttle

---
+++ components/utils/DebounceSelect.tsx
@@ -0,0 +1,68 @@
+import React, { useMemo, useRef, useState } from 'react';
+import { Select, Spin } from 'antd';
+import type { SelectProps } from 'antd/es/select';
+import debounce from 'lodash/debounce';
+import { LoadingOutlined } from '@ant-design/icons';
+
+export interface DebounceSelectProps<ValueType = any>
+  extends Omit<SelectProps<ValueType | ValueType[]>, 'options' | 'children'> {
+  fetchOptions: (search: string) => Promise<ValueType[]>;
+  debounceTimeout?: number;
+}
+
+export function DebounceSelect<
+  ValueType extends {
+    key?: string;
+    label: React.ReactNode;
+    value: string | number;
+  } = any
+>({
+  fetchOptions,
+  debounceTimeout = 800,
+  notFoundContent,
+  ...props
+}: DebounceSelectProps<ValueType>) {
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState<ValueType[]>([]);
+  const fetchRef = useRef(0);
+
+  const debounceFetcher = useMemo(() => {
+    const loadOptions = (value: string) => {
+      fetchRef.current += 1;
+      const fetchId = fetchRef.current;
+      setOptions([]);
+      setFetching(true);
+
+      fetchOptions(value).then((newOptions) => {
+        if (fetchId !== fetchRef.current) {
+          // for fetch callback order
+          return;
+        }
+
+        setOptions(newOptions);
+        setFetching(false);
+      });
+    };
+
+    return debounce(loadOptions, debounceTimeout);
+  }, [fetchOptions, debounceTimeout]);
+
+  return (
+    <Select
+    allowClear
+      showSearch
+      labelInValue
+      filterOption={false}
+      onSearch={debounceFetcher}
+      notFoundContent={
+        fetching ? (
+          <LoadingOutlined spin className='flex justify-center items-center' />
+        ) : (
+          notFoundContent || null
+        )
+      }
+      {...props}
+      options={options}
+    />
+  );
+}
